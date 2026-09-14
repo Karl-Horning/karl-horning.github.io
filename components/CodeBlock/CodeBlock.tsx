61fb722844a7dace21a2eae2530cf4701f631e0b
Karl Horning <k.horning@me.com>
2026-09-14 08:22:25 +0100

    a11y: increase comment contrast in codeblocks

--- components/CodeBlock/CodeBlock.tsx
+++ components/CodeBlock/CodeBlock.tsx
@@ -35,7 +35,7 @@ export default async function CodeBlock({
         lang,
         themes: {
             light: "github-light",
-            dark: "github-dark",
+            dark: "github-dark-default",
         },
     });
 
